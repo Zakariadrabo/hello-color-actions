74d56c746cca603f00cd3ee4d1085b237fd94aec
zakaria.drabo <zakaria.drabo@amf-umoa.org>
2024-06-08 22:43:11 +0000

    ajout nom

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from the Office Hours in the terminal! My Name Is Zakaria DRABO";
 console.log(hello());
 exports.hello = hello;
 
